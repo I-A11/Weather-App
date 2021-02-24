399795d17c50c4d01ebc303743a9d5ae172d235b
I-A11 <d.islam1983@gmail.com>
2021-02-24 18:00:22 +1100

    weather app

---
+++ index.js
@@ -0,0 +1,31 @@
+const appId = 'Get your Id';
+const creatCardHtml = (name, temp, feelsLike, description) =>
+    `
+<div class="card-body ms-4 pt-3">
+<h5 class="card-title">${name}</h5>
+<h5 class="card-subtitle mb-2 text-muted">${temp}c</h6>
+    <p class="card-text"> Feels like ${feelsLike}</p>
+    <p class="card-text">${description}</p>
+`;
+
+const cityInput = document.querySelector('#city-input');
+const submitButton = document.querySelector('#submit-button');
+const tempCard = document.querySelector('#temp-card');
+
+
+submitButton.addEventListener('click', async () => {
+    const cityInputValue = cityInput.value;
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInputValue}&appid=${appId}&units=metric`);
+    const data = await response.json();
+
+
+    const name = data.name;
+    const temp = data.main.temp;
+    const feelsLike = data.main.feels_like;
+    const description = data.weather[0].description;
+
+
+    const finishedHtml = (creatCardHtml(name, temp, feelsLike, description));
+    tempCard.innerHTML = finishedHtml;
+});
